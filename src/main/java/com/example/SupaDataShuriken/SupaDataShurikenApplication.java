d7db086310fd52773a56301cc10409e966c1c646
Prince Anand <anandprince0602@gmail.com>
2024-04-28 12:32:20 +0530

    Initial commit

---
+++ src/main/java/com/example/SupaDataShuriken/SupaDataShurikenApplication.java
@@ -0,0 +1,13 @@
+package com.example.SupaDataShuriken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupaDataShurikenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupaDataShurikenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SupaDataShuriken/SupaDataShurikenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SupaDataShuriken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupaDataShurikenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
